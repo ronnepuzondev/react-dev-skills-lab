caa634245a15b27480f8f68956817304b0c70019
rmap88 <ronnepdev@gmail.com>
2022-11-16 16:49:26 -0800

    add state hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./Onefile.css"
 import SkillList from "./SkillList"
 import NewSkillForm from "./NewSkillForm"
+import { useState } from 'react'
 
 
 
 function App() {
-  const skills = [
+
+  const [skills, addSkills] = useState([
     { name: "HTML", level: 5 },
     { name: "CSS", level: 3 },
     { name: "JavaScript", level: 4 },
     { name: "Python", level: 2 },
-  ];
+  ])
   return (
     <div>
       <h1>React Dev Skills</h1>
