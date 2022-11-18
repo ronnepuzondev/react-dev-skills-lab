3e266d0cce776e0a088f3cc98df7096ca365a24e
rmap88 <ronnepdev@gmail.com>
2022-11-17 17:24:27 -0800

    close to end of part 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,29 @@ import "./Onefile.css"
 import SkillList from "./SkillList"
 import NewSkillForm from "./NewSkillForm"
 import { useState } from 'react'
+import handleAddSkill from "./NewSkillForm"
 
 
 
 function App() {
 
-  const [skills, addSkills] = useState([
+  const [skills, setSkills] = useState([
     { name: "HTML", level: 5 },
     { name: "CSS", level: 3 },
     { name: "JavaScript", level: 4 },
     { name: "Python", level: 2 },
   ])
+
+ function addSkill(skill) {
+  const updatedState = {...skills, skill}
+  setSkills(updatedState)
+ }
+  
   return (
     <div className ="App">
       <h1>React Dev Skills</h1>
       <SkillList skills={skills} />
-      <NewSkillForm />
+      <NewSkillForm addSkill={addSkill} />
     </div>
     
   );
--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,25 +1,49 @@
-import "./Onefile.css"
+import { useState } from "react";
+// import App from "./App";
+// import skills from "./App";
+// import setSkills from "./App";
 
-function NewSkillForm() {
-    return (
-        <div>
-        <form className="NewSkillForm">
+function NewSkillForm({addSkill}) {
+  const [newSkill, setNewSkill] = useState([{ name: "", level: 3 }]);
+
+  const handleAddSkill = (event) => {
+    event.preventDefault();
+    addSkill(newSkill);
+    setNewSkill({ name: "", level: 3 });
+  };
+
+//   const handleSkillChange = (event) => {
+//     setNewSkill(event.target.value);
+//   };
+
+  const handleChange = (event) => {
+    const updatedState = {...newSkill, [event.target.name]:event.target.value}
+    setNewSkill(updatedState)
+  }
+
+  return (
+    <div>
+      <form className="NewSkillForm" onSubmit={handleAddSkill}>
         <label for="skill">Skill</label>
-        <input className="input" type="text" name="skill"></input>
+        <input
+          value={newSkill}
+          onChange={handleChange}
+          className="input"
+          type="text"
+          name="name"
+        ></input>
         <label for="Level">Level</label>
-        <select className="select" name="level">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    </select>
-    <button type="submit">ADD SKILL</button>
-        </form>
-        </div>
-    
-
-    )
+        <select className="select" name="level" onChange={handleChange}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+        <button type="submit">ADD SKILL</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewSkillForm;
